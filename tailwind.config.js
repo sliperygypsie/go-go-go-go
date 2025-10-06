b5d20d6892f6d88544fa7ad99b63588f2e2b0fe1
RO1B666 <robbolowr@gmail.com>
2025-10-06 12:33:10 +1100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        'out': {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+      },
+      animation: {
+        'in': 'in 0.2s ease-out',
+        'out': 'out 0.2s ease-in',
+      },
+    },
   },
   plugins: [],
 };
